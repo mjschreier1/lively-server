cc915dd54596da67cdcddbc4ca0f5d71016b2d29
Michael Schreier <mjschreier1@gmail.com>
2018-05-08 18:37:32 -0600

    initial commit

---
+++ src/main/java/com/lively/LiveLy/LiveLyApplication.java
@@ -0,0 +1,12 @@
+package com.lively.LiveLy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveLyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveLyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lively/LiveLy/LiveLyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lively.LiveLy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiveLyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
