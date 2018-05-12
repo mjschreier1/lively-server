58b59141612d8e847ae77e01605e609e96150f1a
Michael Schreier <mjschreier1@gmail.com>
2018-05-12 15:02:25 -0600

    fixed error in event repository

--- src/main/java/com/lively/LiveLy/repo/EventRepository.java
+++ src/main/java/com/lively/LiveLy/repo/EventRepository.java
@@ -9,9 +9,4 @@ import java.util.List;
 public interface EventRepository extends CrudRepository<Event, Long> {
     List<Event> findById(long id);
     List<Event> findByFinishAfter(LocalDateTime now);
-
-    @Override
-    protected void configureRepositoryRestConfiguration(RepositoryRes config) {
-        config.exposeIdsFor(Person.class);
-    }
 }
