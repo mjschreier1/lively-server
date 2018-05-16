36526557162026f2c6194650a58a969c9d184370
Michael Schreier <mjschreier1@gmail.com>
2018-05-16 09:20:02 -0600

    changed to Boolean object typing for successful column of payments table

--- src/main/java/com/lively/LiveLy/model/Payment.java
+++ src/main/java/com/lively/LiveLy/model/Payment.java
@@ -21,7 +21,7 @@ public class Payment {
     private LocalDateTime submittedOn;
 
     @Column(name = "successful")
-    private boolean successful;
+    private Boolean successful;
 
     protected Payment() {
 
@@ -61,11 +61,11 @@ public class Payment {
         this.submittedOn = submittedOn;
     }
 
-    public boolean isSuccessful() {
+    public Boolean isSuccessful() {
         return successful;
     }
 
-    public void setSuccessful(boolean successful) {
+    public void setSuccessful(Boolean successful) {
         this.successful = successful;
     }
 }
